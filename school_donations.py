bc3fa27bb68bcd438f93f78e9fc141c07b304aea
cormacio100 <cormac.music@gmail.com>
2017-04-05 00:08:26 +0100

    Connect to MongoDB

--- school_donations.py
+++ school_donations.py
@@ -2,12 +2,23 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'donorsUSA'
+'''
+REMOTE HEROKU SETTINGS
+'''
+MONGO_URI = os.getenv('MONGODB_URI','mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME','donorUSA')
+
+'''
+LOCAL SETTINGS
+'''
+#MONGODB_HOST = 'localhost'
+#MONGODB_PORT = 27017
+#DBS_NAME = 'donorsUSA'
+
 COLLECTION_NAME = 'projects'
 
 
@@ -40,14 +51,30 @@ def donor_projects():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+
+    '''
+    REMOTE HEROKU DB SETTINGS
+    '''
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
         # and limit the the results to 55000
-        projects = collection.find(projection=FIELDS, limit=55000)
+        projects = collection.find(projection=FIELDS, limit=20000)
         # Convert projects to a list in a JSON object and return the JSON data
         return json.dumps(list(projects))
+
+    '''
+    LOCAL DB SETTTINGS
+    '''
+    #with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        # Define which collection we wish to access
+    #   collection = conn[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # and limit the the results to 55000
+    #    projects = collection.find(projection=FIELDS, limit=55000)
+        # Convert projects to a list in a JSON object and return the JSON data
+    #   return json.dumps(list(projects))
 
 
 if __name__ == "__main__":
